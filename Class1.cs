3fb5ab3b15f7141decc5e99240275605c2f14198
sborkar009 <141212389+sborkar009@users.noreply.github.com>
2023-08-21 18:52:53 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class VName
+{
+    public int VNameId { get; set; }
+    public string OwnerName { get; set; }
+    public string ContactNo { get; set; }
+    public string Location { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+    public VName(string ownerName, string contactNo, string location)
+    {
+        OwnerName = ownerName;
+        ContactNo = contactNo;
+        Location = location;
+        CreatedOn = DateTime.Now;
+    }
+}
+
+public class Vehicle
+{
+    public int VehicleId { get; set; }
+    public string VehicleNoPlate { get; set; }
+    public int VNameId { get; set; }
+    public string VehicleName { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+    public Vehicle(string vehicleNoPlate, int vNameId, string vehicleName)
+    {
+        VehicleNoPlate = vehicleNoPlate;
+        VNameId = vNameId;
+        VehicleName = vehicleName;
+        CreatedOn = DateTime.Now;
+    }
+}
+
+public class ParkingTicket
+{
+    public int ParkingTicketId { get; set; }
+    public int VehicleId { get; set; }
+    public int VNameId { get; set; }
+    public DateTime IssueDate { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+    public ParkingTicket(int vehicleId, int vNameId, DateTime issueDate, decimal amount)
+    {
+        VehicleId = vehicleId;
+        VNameId = vNameId;
+        IssueDate = issueDate;
+        Amount = amount;
+        CreatedOn = DateTime.Now;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amazon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbi bank = new sbi()
+            {
+                userid = "sborkar009",
+                enter_amount = 2000
+
+            };
+            Console.WriteLine(bank.userid);
+            Console.WriteLine(bank.enter_amount);
+            Console.ReadKey();
+        }
+        public class sbi
+        {
+            public string userid { get; set; }
+            private string password { get; set; }
+
+            public int enter_amount { get; set; }
+            
+        }
+    }   
+}   
